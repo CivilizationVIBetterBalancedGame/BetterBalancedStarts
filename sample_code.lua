a104d43ae1368547eebfb5c035e3ff2670ec77b8
Talasta <maxime.choulika@gmail.com>
2023-06-29 15:40:27 +0200

    add min distance calculation

--- sample_code.lua
+++ sample_code.lua
@@ -1,7 +1,51 @@
+---------------------------------------------------------------------------------
+-- definition of Globals --------------------------------------------------------
+---------------------------------------------------------------------------------
+
+-- Min distance definition
+local bbs_mapTypeToDistance = {};
+bbs_mapTypeToDistance["Highlands_XP2.lua"] = 15
+bbs_mapTypeToDistance["Lakes.lua"] = 15
+bbs_mapTypeToDistance["InlandSea.lua"] = 14
+bbs_mapTypeToDistance["Seven_Seas.lua"] = 13
+bbs_mapTypeToDistance["Primordial.lua"] = 13
+bbs_mapTypeToDistance["Pangaea.lua"] = 12
+bbs_mapTypeToDistance["DWPangaea.lua"] = 12
+bbs_mapTypeToDistance["Shuffle.lua"] = 12
+bbs_mapTypeToDistance["Tilted_Axis.lua"] = 12
+bbs_mapTypeToDistance["Tilted_Axis.lua"] = 12
+bbs_mapTypeToDistance["Fractal.lua"] = 10
+bbs_mapTypeToDistance["Island_Plates.lua"] = 10
+bbs_mapTypeToDistance["Small_Continents.lua"] = 10
+bbs_mapTypeToDistance["Archipelago_XP2.lua" ] = 10
+bbs_mapTypeToDistance["Continents.lua"] = 10
+bbs_mapTypeToDistance["Wetlands_XP2.lua"] = 10
+bbs_mapTypeToDistance["Continents_Islands.lua"] = 10
+bbs_mapTypeToDistance["Splintered_Fractal.lua"] = 10
+bbs_mapTypeToDistance["DWArchipelago.lua"] = 10
+bbs_mapTypeToDistance["DWFractal.lua"] = 10
+bbs_mapTypeToDistance["DWMixedLand.lua"] = 10
+bbs_mapTypeToDistance["DWSmallContinents.lua"] = 10
+bbs_mapTypeToDistance["DWMixedIslands.lua"] = 10
+bbs_mapTypeToDistance["Terra.lua"] = 8
+
+-- Special civilisation types
+BBS_LEADER_TYPE_SPECTATOR = "LEADER_SPECTATOR"
+
+---------------------------------------------------------------------------------
+-- Gather base game info and setup config ---------------------------------------
+---------------------------------------------------------------------------------
+
 -- Get map size and min distance data
 local Width, Height = Map.GetGridSize();
 local minDistance = MapConfiguration.GetValue("BBSMinDistance");
--- Setmin distance if minDistance is nil.
+local mapScript = MapConfiguration.GetValue("MAP_SCRIPT");
+-- Set min distance if minDistance is nil or 0
+if ((minDistance == nil or minDistance == 0) and mapScript ~= nil) then
+    minDistance = bbs_mapTypeToDistance[mapScript]
+end
+-- Adjust min distance in function of player number
+minDistance = minDistance + BBS_DistanceFromPlayerCount()
 
 -- Get map type
 
@@ -13,11 +57,11 @@ for i = 1, PlayerManager.GetAliveMajorsCount() do
     local leader = PlayerConfigurations[civilisationsIDs[i]]:GetLeaderTypeName();
     local name = PlayerConfigurations[civilisationsIDs[i]]:GetCivilizationTypeName();
     local team = Players[civilisationsIDs[i]]:GetTeam();
-    civilisations[i] = {
+    table.insert(civilisations, {
         leader = leader,
         name = name,
         team = team,
-    };
+    });
 end
 
 -- Get City States data
@@ -61,3 +105,21 @@ for key , value in pairs(ret) do
     end
 end
 
+function BBS_DistanceFromPlayerCount()
+    local civilisationsIDs = PlayerManager.GetAliveMajorIDs();
+    local totalPlayers = 0
+    for i = 1, PlayerManager.GetAliveMajorsCount() do
+        player = PlayerConfigurations[civilisationsIDs[i]]
+        if (player:GetLeaderTypeName() ~= BBS_LEADER_TYPE_SPECTATOR and player:GetHandicapTypeID() ~= 2021024770) then
+            totalPlayers = totalPlayers + 1
+        end
+    end
+    local mapSize = Map.GetMapSize();
+    local numberPlayerSettings = (mapSize + 1) * 2
+    if totalPlayers > numberPlayerSettings then
+        return -2;
+    elseif totalPlayers < numberPlayerSettings then
+        return 2
+    end
+    return 0
+end
