2f57bd5942a808a4a2e346eb8eed9964611d0810
banipouz <banipouz@gmail.com>
2024-03-15 00:16:16 +0100

    V0.21

--- UI/BBS_UI.lua
+++ UI/BBS_UI.lua
@@ -11,7 +11,7 @@ function OnLocalPlayerTurnBegin()
 	end
 	if Game.GetCurrentGameTurn() == GameConfiguration.GetStartTurn() then
 		print("BBS UI Welcome")
-		local message = "BBS #"..GetLocalModVersion("c88cba8b-8311-4d35-90c3-51a4a5d66542").." loaded succesfully!"
+		local message = "BBM #"..GetLocalModVersion("c88cba8b-8311-4d35-90c3-51a4a5d66542").." loaded succesfully!"
 
 		if Game:GetProperty("BBS_TEAMERCONTINENTCHECK") ~= nil and Game:GetProperty("BBS_TEAMERCONTINENTCHECK") then
 			print("BBS_TEAMERCONTINENTCHECK - ", Game:GetProperty("BBS_TEAMERCONTINENTCHECK"))
