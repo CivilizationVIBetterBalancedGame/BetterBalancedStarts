df79d02b134bf33cb364d740f941cebb9723a7d6
banipouz <banipouz@gmail.com>
2024-11-11 19:52:24 +0100

    V1.0

--- UI/BBM_UI.lua
+++ UI/BBM_UI.lua
@@ -1,4 +1,4 @@
-local g_version = "0.95" --TODO fix get version from modinfo
+local g_version = "1.00" --TODO fix get version from modinfo
 -------------------------------------------------------------------------------
 print("-------------- BBM UI v"..g_version.." -D- Init --------------")
 -------------------------------------------------------------------------------
