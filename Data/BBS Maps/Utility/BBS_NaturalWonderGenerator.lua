9a9c6cef4172718c1d75fec8905254db5c6a9a8a
BigBill0 <joshgreenspan0@gmail.com>
2025-07-25 10:11:58 -0400

    removed other debug statement i added while checking some things

--- Data/BBS Maps/Utility/BBS_NaturalWonderGenerator.lua
+++ Data/BBS Maps/Utility/BBS_NaturalWonderGenerator.lua
@@ -265,6 +265,7 @@ function BBS_NaturalWonderGenerator:__ScorePlots(NWIndex)
 		else
 			iDistanceScore = 1500 + (iClosestConflictingDist - 10);
 		end
+		
 		row.Score = iDistanceScore + TerrainBuilder.GetRandomNumber(100, "Natural Wonder Placement Score Adjust");
 	end
 end
